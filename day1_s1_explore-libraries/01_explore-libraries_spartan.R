4e07917ac87bbb1bd33eb1aea0fe918d2b84909d
Jessica Minnier <jessica.minnier@gmail.com>
2018-01-31 11:01:35 -0800

    morning exercise

--- day1_s1_explore-libraries/01_explore-libraries_spartan.R
+++ day1_s1_explore-libraries/01_explore-libraries_spartan.R
@@ -2,10 +2,14 @@ library(tidyverse)
 library(janitor)
 
 
-
 #' Which libraries does R search for packages?
 .Library
 .libPaths()
+library(fs)
+path_real(.Library) # this actually is my true path, the .Library result is a symbolic link!!
+
+#' - reminder to use file.path() or other functions that are made to work with paths, not paste()
+#' - jenny is learning this new package `fs` with us
 
 #' Installed packages
 
@@ -20,10 +24,10 @@ nrow(ip)
 
 ## count some things! inspiration
 ##   * tabulate by LibPath, Priority, or both
-ip%>%group_by(LibPath)%>%count()
-ip%>%group_by(LibPath,Priority)%>%count()
+ip%>%count(LibPath)
+ip%>%count(LibPath,Priority)
 ##   * what proportion need compilation?
-ip%>%tabyl(NeedsCompilation)
+ip%>%tabyl(NeedsCompilation) # jenny uses count and then mutate to calculate a prop = n/sum(n)
 ##   * how break down re: version of R they were built on
 ip%>%tabyl(Built)
 ip%>%filter(Built=="3.0.0")
@@ -42,9 +46,26 @@ ip%>%filter(Built=="3.0.0")
 ## if you have time to do more ...
 
 ## is every package in .Library either base or recommended? I have no packages in .Library
+all_default_pkgs <- list.files(.Library)
+all_br_pkgs <- ip %>%
+  filter(Priority %in% c("base", "recommended")) %>%
+  pull(Package)
+setdiff(all_default_pkgs, all_br_pkgs)[1:10]
+
 ## study package naming style (all lower case, contains '.', etc
 ip%>%mutate(has_dot = str_detect(Package,pattern=fixed(".")),
             has_underscore = str_detect(Package,pattern=fixed("_")),
             all_lowercase = str_to_lower(Package)==Package)%>%
   count(has_dot,has_underscore,all_lowercase)
 ## use `fields` argument to installed.packages() to get more info and use it!
+ipf <- installed.packages(fields = c("License","Author"))
+ipf <- tbl_df(ipf)
+ipf%>%tabyl(License)%>%arrange(desc(n))
+
+#' How many packages are by one of the Wickhams?
+ipf <- ipf%>%mutate(by_hadley = str_detect(Author,"Hadley"),
+             by_wickham = str_detect(Author,"Wickham"))
+ipf%>%count(by_hadley,by_wickham)
+#' Where's Charlotte?
+ipf%>%filter((!by_hadley)&by_wickham)%>%pull(Author)
+ipf%>%filter((!by_hadley)&by_wickham)%>%pull(Package)
